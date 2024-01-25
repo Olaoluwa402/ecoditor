4b5728fe3645214e55726931b4f9e2c69ca62bd5
Olaoluwa402 <ibukundaniel402@yahoo.co.uk>
2024-01-25 14:00:55 +0100

    init project setup

---
+++ backend/server.js
@@ -0,0 +1,7 @@
+import express from "express";
+
+const app = express();
+
+app.listen(8001, () => {
+  console.log("app running");
+});
---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h2>Code Editor</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
